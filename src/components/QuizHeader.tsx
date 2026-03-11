aee582a02337c344482e6ff2de5b70e2a12b9bd3
vu.dc.fe <vudz2310@gmail.com>
2026-03-11 19:51:46 +0700

    fix: xoa popup name

--- src/components/QuizHeader.tsx
+++ src/components/QuizHeader.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-const NameUser = localStorage.getItem("nameUser") || "User";
 export function QuizHeader() {
   return (
     <div className="header">
       <h1>Quiz Editor</h1>
       <p>Create and manage your quizzes</p>
-      <p>Welcome, {NameUser}!</p> 
       <h1></h1>
     </div>
   );
